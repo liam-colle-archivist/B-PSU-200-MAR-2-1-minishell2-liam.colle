d10d6656215e5c08600726e46382609fda3024b3
Liam Colle <liam.colle@epitech.eu>
2025-03-17 14:03:43 +0100

    fix: Fix coding style checker wrapper shell file

--- tests/action_utils/cs_check.sh
+++ tests/action_utils/cs_check.sh
@@ -8,7 +8,7 @@
 
 while IFS=': '; read -r -a array
 do
-    echo "::error file={${array[0]}},line={${array[1]}},endLine={${array[1]}},title={${array[2]} coding style error}::{${array[3]}}"
+    echo "::error file=${array[0]},line=${array[1]},endLine=${array[1]},title=${array[2]} coding style error::${array[3]}"
 done < coding-style-reports.log
 
 if [ -s coding-style-reports.log ]; then
